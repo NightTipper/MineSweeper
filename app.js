ac50b26ac5bd3aeb23a82308d66fb53d122e9b4d
NightTipper <126615601+NightTipper@users.noreply.github.com>
2023-09-03 21:30:56 +0100

    Updated the game grid, and allow the game to expand and grow when different game difficulties have been selected.

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 //variables
 let gameGrid = []
-let rows = 8;
-let columns = 8;
+let rows = 0;
+let columns = 0;
+let gameGridWidth = 0;
 let minesCount = 10;
 let minesLoc = [];
 let tilesClick = 0;
@@ -20,16 +21,26 @@ function updateLevelSettings() {
         columns = 16;
         rows = 36;
         minesCount = 140;
+        gameGridWidth = '800px'
+        gameGridHeight = '800px'
     } else if (selectedDifficulty === "Advance") {
         columns = 16;
         rows = 16;
         minesCount = 40;
+        gameGridWidth = '800px'
+        gameGridHeight = '800px'
     } else if (selectedDifficulty === "Expert") {
         minesCount = 90;
         columns = 16;
         rows = 30;
+        gameGridWidth = '800px'
+        gameGridHeight = '800px'
     } else {
         minesCount = 10;
+        rows = 8;
+        columns = 8;
+        gameGridWidth = '400px';
+        gameGridHeight = '400px';
     }
 };
 
@@ -54,9 +65,12 @@ function startGame() {
 
     //Display the new game:
     document.getElementById('StartedGame').style.display = 'block';
+    document.getElementById('gameButtons').style.display = 'block';
     document.getElementById('gameStart').style.display = 'flex';
     document.getElementById('MineCounter').innerHTML = minesCount; 
 
+    document.getElementById('gameGrid').style.width = gameGridWidth; 
+    document.getElementById('gameGrid').style.width = gameGridHeight; 
 
     //Create the tiles:
     for (let r = 0; r < rows; r++) {
@@ -77,10 +91,22 @@ function restartGame() {
     //hide the game
     document.getElementById('StartedGame').style.display = 'none';
     document.getElementById('gameStart').style.display = 'none';
+    document.getElementById('gameButtons').style.display = 'none';
 
     //show the instructions
     document.getElementById('DiffLevelContainer').style.display = 'block';
-    document.getElementById('PlayButton').style.display = 'block';
+    document.getElementById('PlayButton').style.display = 'inline-block';
+
+    //reset the game:
+    gameGrid = document.getElementById('gameGrid');
+    gameGrid.remove();
+    newGrid = document.createElement("div");
+    newGrid.id = "gameGrid";
+    document.getElementById("gameStart").append(newGrid);
+    rows = 0;
+    columns = 0;
+    gameGrid = [];
+
 }
 
 
